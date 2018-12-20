7325309c5df546dea140f4ea6b5bc9edf3efb904
Thomas Lebeau <lebeau.thomas@gmail.com>
2018-12-20 11:28:44 +0100

    feat: add window.__version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './style';
 import App from './components/app';
 
+import { version } from '../package.json';
+
+if (typeof window !== 'undefined') {
+  window.__version = version;
+}
+
 export default App;
