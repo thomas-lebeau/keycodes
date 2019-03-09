c2c610955b747e61d89cf32712bdb836a8ea87d1
Thomas Lebeau <lebeau.thomas@gmail.com>
2019-03-09 21:28:16 +0100

    chore: Replace preact-cli with parcel-bundler

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,46 +1,49 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 
-import { Help } from './help';
-import { Undef } from './undef';
-import { Code } from './code';
-import { useEventListener } from '../hooks/useEventListener';
+import Help from './help';
+import Undef from './undef';
+import Code from './code';
+import useEventListener from '../hooks/useEventListener';
 
-export const App = ()  => {
-  const [codes, setCodes] = useState([]);
-  const [copied, setCopied] = useState(false);
+export default function App() {
+    const [codes, setCodes] = useState([]);
+    const [copied, setCopied] = useState(false);
 
-  const handleCopy = name => setCopied(name);
-  const handleKeydown = ({ code, key, keyCode }) => {
-    setCopied(false);
-    setCodes({
-      code,
-      key,
-      keyCode,
-    })
-  }
+    const handleCopy = name => setCopied(name);
 
-  useEventListener('keydown', handleKeydown);
+    const handleKeydown = ({ code, key, keyCode }) => {
+        setCopied(false);
+        setCodes({
+            code,
+            key,
+            keyCode,
+        });
+    };
 
-  return (
-    <main>
-      <span class="operator"> const </span>event<span class="operator"> = </span>
-      {codes ? '{' : <Undef />}
-      <ul>
-        {Object.entries(codes).map(c => (
-          <Code
-            name={c[0]}
-            code={c[1]}
+    const renderCode = ([name, code]) => (
+        <Code
+            key={name}
+            name={name}
+            code={code}
             onCopy={handleCopy}
-            isCopied={c[0] === copied}
-          />
-        ))}
-        {codes && <li class="comment">&nbsp;&nbsp;// ...</li>}
-      </ul>
-      {codes && '};'}
-      <Help />
-    </main>
-  );
-}
+            isCopied={name === copied}
+        />
+    );
 
-export default App;
+    useEventListener('keydown', handleKeydown);
+
+    return (
+        <Fragment>
+            <span className="operator"> const </span>event
+            <span className="operator"> = </span>
+            {codes ? '{' : <Undef />}
+            <ul>
+                {Object.entries(codes).map(renderCode)}
+                {codes && <li className="comment">&nbsp;&nbsp;// ...</li>}
+            </ul>
+            {codes && '};'}
+            <Help />
+        </Fragment>
+    );
+}
--- src/components/code.jsx
+++ src/components/code.jsx
@@ -1,20 +1,37 @@
+import { h } from 'preact';
+import PropTypes from 'prop-types';
 import copy from 'copy-text-to-clipboard';
 
-export const Code  = ({ name, code, isCopied, onCopy }) => {
-  const handleClick = () => {
-    copy(code.toString());
-    onCopy(name);
-  }
+export default function Code({ name, code, isCopied, onCopy }) {
+    const handleClick = () => {
+        copy(code.toString());
+        onCopy(name);
+    };
 
-  return (
-    <li>
-      &nbsp;&nbsp;
-      {name}
-      <span class="operator">: </span>
-      <span class="string hoverable" onClick={handleClick} tabIndex="0">
-        '{code}'
-      </span>,
-      {isCopied && <span class="comment"> // Copied to clipboard</span>}
-    </li>
-  );
+    return (
+        <li>
+            &nbsp;&nbsp;
+            {name}
+            <span className="operator">: </span>
+            <button
+                type="button"
+                className="string hoverable"
+                onClick={handleClick}
+                tabIndex="0"
+            >
+                &apos;{code}&apos;
+            </button>
+            ,
+            {isCopied && (
+                <span className="comment"> // Copied to clipboard</span>
+            )}
+        </li>
+    );
+}
+
+Code.propTypes = {
+    name: PropTypes.string.isRequired,
+    code: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    isCopied: PropTypes.bool.isRequired,
+    onCopy: PropTypes.func.isRequired,
 };
